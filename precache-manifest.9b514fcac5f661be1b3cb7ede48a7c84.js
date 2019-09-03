0502550d3f26acdffe0c8dac1d7f8f45fc0b6111
piatek29535 <adamex12343@gmail.com>
2019-09-03 14:31:20 +0200

    Updates

---
+++ precache-manifest.9b514fcac5f661be1b3cb7ede48a7c84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb57d4bdf93c8b7fdafb36fc5e7d312f",
+    "url": "/floofer-frontend/index.html"
+  },
+  {
+    "revision": "a18fee7d0084d8c2edaa",
+    "url": "/floofer-frontend/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ac171a38a3f829966bc7",
+    "url": "/floofer-frontend/static/js/2.24b27997.chunk.js"
+  },
+  {
+    "revision": "a18fee7d0084d8c2edaa",
+    "url": "/floofer-frontend/static/js/main.4d253038.chunk.js"
+  },
+  {
+    "revision": "3cab8ed62cab68322e04",
+    "url": "/floofer-frontend/static/js/runtime~main.e4552e17.js"
+  }
+]);
