0768d386771b3d6a65b50a954a6bdd24e813397c
piatek29535 <adamex12343@gmail.com>
2019-10-16 12:37:42 +0200

    Prepared the news screen -- redux next

--- src/components/mainScreen/mainScreenComponents/News.js
+++ src/components/mainScreen/mainScreenComponents/News.js
@@ -1,12 +1,73 @@
 import React, {Component} from 'react';
 import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItem from "@material-ui/core/ListItem";
+import Container from "@material-ui/core/Container";
+
+
+const styles={
+    mainContainer:{
+        height:'100%',
+    },
+    mainContainerTypography:{
+        width:'90%',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        borderBottom:'1px solid grey',
+        marginBottom:'10px'
+    },
+    list:{
+        maxHeight:'90%',
+        overflow:'auto'
+    },
+    listItem:{
+        borderRadius:'10px',
+        padding:'5px',
+        margin:'1%',
+        boxShadow:'0 0 10px',
+        maxWidth:'97%'
+    },
+};
 
 class News extends Component {
+
     render() {
         return (
-            <Typography variant="h5" component="h3">
-                News goes here!
-            </Typography>
+            <div style={styles.mainContainer}>
+                <Container style={styles.mainContainerTypography}>
+                    <Typography variant={"h4"}>
+                        Aktualności
+                    </Typography>
+                </Container>
+                <List style={styles.list}>
+                    {
+                        [{name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                            {name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                            {name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                            {name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                            {name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                            {name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                            {name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                            {name:'Name and Surename', desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"},
+                        ].map((item, key) => (
+                            <ListItem key={key} style={styles.listItem} alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt=" " src="https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-256.png" />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={item.name}
+                                    secondary={item.desc}
+                                    //this should contain the real fetched data from server
+                                />
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </div>
         );
     }
 }
